820a692d2e25364d68d2957d581036f237c39671
sofials <108076396+sofials@users.noreply.github.com>
2025-05-22 17:59:10 +0200

    prove effetto visivo attacco player
    prova, niente di definitivo

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject attackHitbox;
+    public ParticleSystem windEffect;  // effetto aria mossa dallo spin
+
     private Animator animator;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         DeactivateHitbox();
+        StopWindEffect();
     }
 
     private void Update()
@@ -19,16 +22,35 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
-    // Chiamato dall'evento nell'animazione
+    // Chiamato dall'evento nell'animazione all'inizio dell'attacco
     public void ActivateHitbox()
     {
         attackHitbox.SetActive(true);
         Debug.Log("Hitbox ATTIVA");
+        PlayWindEffect();
     }
 
+    // Chiamato dall'evento nell'animazione alla fine dell'attacco
     public void DeactivateHitbox()
     {
         attackHitbox.SetActive(false);
         Debug.Log("Hitbox DISATTIVATA");
+        StopWindEffect();
+    }
+
+    private void PlayWindEffect()
+    {
+        if (windEffect != null && !windEffect.isPlaying)
+        {
+            windEffect.Play();
+        }
+    }
+
+    private void StopWindEffect()
+    {
+        if (windEffect != null && windEffect.isPlaying)
+        {
+            windEffect.Stop();
+        }
     }
 }
