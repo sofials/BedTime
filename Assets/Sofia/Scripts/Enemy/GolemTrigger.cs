3fa8a9102e4acfd4d1601128771e2b85bc78cb8a
sofials <108076396+sofials@users.noreply.github.com>
2025-07-13 23:31:46 +0200

    cose

--- Assets/Sofia/Scripts/Enemy/GolemTrigger.cs
+++ Assets/Sofia/Scripts/Enemy/GolemTrigger.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
 public class GolemTrigger : MonoBehaviour
-{
-    public Golem golem;
+{  
+    public GolemVillaggio golem;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-          
+            
+
+           if (golem != null)
+{
+    golem.ActivateWalk();
+}
+
+            else
+            {
+                Debug.LogWarning("Nessun GolemVillaggio trovato nella scena.");
+            }
         }
     }
 }
